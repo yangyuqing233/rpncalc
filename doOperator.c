00b4d44c167b5448ab1f2537b8c1b1f33d5bc459
yangyuqing233 <43785074+yangyuqing233@users.noreply.github.com>
2018-11-11 18:52:00 -0500

    Update doOperator.c

--- doOperator.c
+++ doOperator.c
@@ -9,6 +9,16 @@ static int op_quit(struct tokenStack *stack);
 static int op_print(struct tokenStack *stack);
 static int op_dump(struct tokenStack *stack);
 static int op_add(struct tokenStack *stack);
+static int op_subtract(struct tokenStack *stack);
+static int op_multiply(struct tokenStack *stack);
+static int op_divide(struct tokenStack *stack);
+static int op_GThan(struct tokenStack *stack);
+static int op_GEThan(struct tokenStack *stack);
+static int op_LThan(struct tokenStack *stack);
+static int op_LEThan(struct tokenStack *stack);
+static int op_modQuot(struct tokenStack *stack);
+static int op_equal(struct tokenStack *stack);
+static int op_mod(struct tokenStack *stack);
 
 static struct operator_struct {
   char *name;
@@ -18,19 +28,43 @@ static struct operator_struct {
   {"print", op_print},
   {"dump", op_dump},
   {"+", op_add},
+  {"-", op_subtract},
+  {"*", op_multiply},
+  {"/", op_divide},
+  {"GT", op_GThan},
+  {"GE", op_GEThan},
+  {"LT", op_LThan},
+  {"LE", op_LEThan},
+  {"MODQ", op_modQuot},
+  {"MOD", op_mod},
+  {"EQ", op_equal},
   {(char *)NULL, (int(*)(struct tokenStack *)) NULL}
 };
 
 
 /* YOU WRITE THIS */
 static int popInt(struct tokenStack *s)
-{
-  return 0;
+{ int pop;
+  if(s->top==0){
+   fprintf(stderr,"popTokenStack: popping an empty stack, aborting\n");
+		exit(1);
+  }
+ else{
+		value = atoi(popTokenStack(s)->symbol);
+	}
+  return pop;
 }
 
 /* YOU WRITE THIS */
 static void pushInt(struct tokenStack *s, int v)
-{
+{   struct lexToken *token;
+	token = allocToken();
+	token->kind = LEX_TOKEN_NUMBER;
+
+	sprintf(token->symbol, "%d", v);
+	pushTokenStack(s,token);
+
+	op_print(s);
 }
 
 int doOperator(struct tokenStack *stack, char *o) 
@@ -75,3 +109,121 @@ static int op_add(struct tokenStack *stack)
   pushInt(stack, v1+v2);
   return(0);
 }
+static int op_subtract(struct tokenStack *stack)
+{
+  int v1, v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  pushInt(stack, v2-v1);
+  return(0);
+}
+
+static int op_multiply(struct tokenStack *stack)
+{
+  int v1, v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  pushInt(stack, v2*v1);
+  return(0);
+}
+
+static int op_divide(struct tokenStack *stack)
+{
+  int v1, v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  pushInt(stack, v2/v1);
+  return(0);
+}
+
+static int op_GThan(struct tokenStack *stack)
+{
+  int v1, v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  if(v2>v1){
+  	pushInt(stack, 1);
+  }
+  else{
+	pushInt(stack, 0);
+  }
+  return(0);
+}
+
+static int op_LThan(struct tokenStack *stack)
+{
+  int v1, v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  if(v2<v1){
+ 	 pushInt(stack, 1);
+  }
+  else{
+	 pushInt(stack, 0);
+  }
+  return(0);
+}
+
+static int op_GEThan(struct tokenStack *stack)
+{
+  int v1, v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  if(v2>=v1){
+ 	 pushInt(stack, 1);
+  }
+  else{
+	 pushInt(stack, 0);
+  }
+  return(0);
+}
+
+static int op_LEThan(struct tokenStack *stack)
+{
+  int v1, v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  if(v2<=v1){
+         pushInt(stack, 1);
+  }
+  else{
+         pushInt(stack, 0);
+  }
+  return(0);
+}
+
+static int op_equal(struct tokenStack *stack)
+{
+  int v1, v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  if(v2==v1){
+         pushInt(stack, 1);
+  }
+  else{
+         pushInt(stack, 0);
+  }
+  return(0);
+}
+
+static int op_mod(struct tokenStack *stack)
+{
+  int v1;
+  v1 = popInt(stack);
+  pushInt(stack, v1);
+  pushInt(stack, v1);
+  return(0);
+}
+
+
+static int op_modQuot(struct tokenStack *stack)
+{
+  int v1;
+  int v2;
+  v1 = popInt(stack);
+  v2 = popInt(stack);
+  pushInt(stack, v2%v1);
+  pushInt(stack, v2/v1);
+  return(0);
+}
+
